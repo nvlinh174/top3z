cbbf56de99b8167275c314e2ac628fdc0f9adfaf
Linh Nguyen <nvlinh17041992@gmail.com>
2026-06-27 15:51:35 +0700

    clean routes

--- routes/web.php
+++ routes/web.php
@@ -31,79 +31,80 @@ Route::get('/robots.txt', function () {
 
 Route::get('/manifest.webmanifest', WebManifestController::class)->name('manifest');
 
-Route::post('/comment-reactions/{comment}/toggle', [CommentReactionController::class, 'toggle'])
-    ->middleware('throttle:60,1')
-    ->whereNumber('comment')
-    ->name('comment-reactions.toggle');
-Route::post('/article-reactions/{article}/toggle', [CommunityReactionController::class, 'toggle'])
-    ->middleware('throttle:60,1')
-    ->whereNumber('article')
-    ->name('article-reactions.toggle');
+Route::middleware('throttle:60,1')->group(function (): void {
+    Route::post('/comment-reactions/{comment}/toggle', [CommentReactionController::class, 'toggle'])
+        ->whereNumber('comment')
+        ->name('comment-reactions.toggle');
+    Route::post('/article-reactions/{article}/toggle', [CommunityReactionController::class, 'toggle'])
+        ->whereNumber('article')
+        ->name('article-reactions.toggle');
+});
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/workshops', [WorkshopController::class, 'index'])->name('workshops.index');
-Route::get('/workshops/{article:slug}', [WorkshopController::class, 'show'])->name('workshops.show');
+Route::prefix('workshops')->group(function (): void {
+    Route::get('/', [WorkshopController::class, 'index'])->name('workshops.index');
+    Route::get('/{article:slug}', [WorkshopController::class, 'show'])->name('workshops.show');
 
-// Mutation routes: module prefix + numeric id (e.g. /article-reactions/{id}/toggle).
-Route::post('/workshops/interests/{article}', [WorkshopInterestController::class, 'store'])
-    ->middleware('throttle:10,1')
-    ->whereNumber('article')
-    ->name('workshops.interest.store');
-Route::post('/workshops/comments', [WorkshopCommentController::class, 'store'])
-    ->middleware('throttle:10,1')
-    ->name('workshops.comments.store');
+    Route::middleware('throttle:10,1')->group(function (): void {
+        Route::post('/interests/{article}', [WorkshopInterestController::class, 'store'])
+            ->whereNumber('article')
+            ->name('workshops.interest.store');
+        Route::post('/comments', [WorkshopCommentController::class, 'store'])->name('workshops.comments.store');
+    });
+});
 
 Route::get('/members/{user}', [MemberProfileController::class, 'show'])->name('members.show');
 
-Route::get('/community', [CommunityController::class, 'index'])->name('community.index');
-Route::post('/community/comments', [CommunityCommentController::class, 'store'])
-    ->middleware('throttle:10,1')
-    ->name('community.comments.store');
+Route::prefix('community')->group(function (): void {
+    Route::get('/', [CommunityController::class, 'index'])->name('community.index');
+    Route::post('/comments', [CommunityCommentController::class, 'store'])
+        ->middleware('throttle:10,1')
+        ->name('community.comments.store');
+});
 
-Route::middleware('auth')->group(function () {
-    Route::patch('/community/comments/{comment}', [CommentController::class, 'update'])
-        ->middleware('throttle:60,1')
-        ->name('community.comments.update');
-    Route::delete('/community/comments/{comment}', [CommentController::class, 'destroy'])
-        ->middleware('throttle:60,1')
-        ->name('community.comments.destroy');
-    Route::get('/community/me', [CommunityPostController::class, 'myPosts'])->name('community.my-posts');
-    Route::get('/community/saved', [CommunitySavedController::class, 'index'])->name('community.saved');
-    Route::get('/community/create', [CommunityPostController::class, 'create'])->name('community.create');
-    Route::post('/community/drafts', [CommunityPostController::class, 'storeDraft'])
-        ->middleware('throttle:30,1')
-        ->name('community.drafts.store');
-    Route::patch('/community/drafts/{article}', [CommunityPostController::class, 'autosaveDraft'])
-        ->middleware('throttle:30,1')
-        ->name('community.drafts.autosave');
-    Route::delete('/community/drafts/{article}', [CommunityPostController::class, 'destroyDraft'])
-        ->middleware('throttle:30,1')
-        ->name('community.drafts.destroy');
-    Route::post('/community', [CommunityPostController::class, 'store'])
-        ->middleware('throttle:10,1')
-        ->name('community.store');
-    Route::get('/community/{article:slug}/edit', [CommunityPostController::class, 'edit'])->name('community.edit');
-    Route::patch('/community/{article:slug}', [CommunityPostController::class, 'update'])
-        ->middleware('throttle:10,1')
-        ->name('community.update');
-    Route::patch('/workshops/comments/{comment}', [CommentController::class, 'update'])
-        ->middleware('throttle:60,1')
-        ->name('workshops.comments.update');
-    Route::delete('/workshops/comments/{comment}', [CommentController::class, 'destroy'])
-        ->middleware('throttle:60,1')
-        ->name('workshops.comments.destroy');
+Route::middleware('auth')->group(function (): void {
+    Route::middleware('throttle:60,1')->group(function (): void {
+        Route::patch('/community/comments/{comment}', [CommentController::class, 'update'])
+            ->name('community.comments.update');
+        Route::delete('/community/comments/{comment}', [CommentController::class, 'destroy'])
+            ->name('community.comments.destroy');
+        Route::patch('/workshops/comments/{comment}', [CommentController::class, 'update'])
+            ->name('workshops.comments.update');
+        Route::delete('/workshops/comments/{comment}', [CommentController::class, 'destroy'])
+            ->name('workshops.comments.destroy');
+    });
+
+    Route::prefix('community')->group(function (): void {
+        Route::get('/me', [CommunityPostController::class, 'myPosts'])->name('community.my-posts');
+        Route::get('/saved', [CommunitySavedController::class, 'index'])->name('community.saved');
+        Route::get('/create', [CommunityPostController::class, 'create'])->name('community.create');
+
+        Route::middleware('throttle:30,1')->group(function (): void {
+            Route::post('/drafts', [CommunityPostController::class, 'storeDraft'])->name('community.drafts.store');
+            Route::patch('/drafts/{article}', [CommunityPostController::class, 'autosaveDraft'])->name('community.drafts.autosave');
+            Route::delete('/drafts/{article}', [CommunityPostController::class, 'destroyDraft'])->name('community.drafts.destroy');
+        });
+
+        Route::post('/', [CommunityPostController::class, 'store'])
+            ->middleware('throttle:10,1')
+            ->name('community.store');
+        Route::get('/{article:slug}/edit', [CommunityPostController::class, 'edit'])->name('community.edit');
+        Route::patch('/{article:slug}', [CommunityPostController::class, 'update'])
+            ->middleware('throttle:10,1')
+            ->name('community.update');
+    });
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::middleware('throttle:60,1')->group(function () {
-        Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.index');
-        Route::get('/notifications/unread-count', [NotificationController::class, 'unreadCount'])->name('notifications.unread-count');
-        Route::get('/notifications/recent', [NotificationController::class, 'recent'])->name('notifications.recent');
-        Route::post('/notifications/read-all', [NotificationController::class, 'markAllRead'])->name('notifications.read-all');
-        Route::post('/notifications/{id}/read', [NotificationController::class, 'markRead'])->name('notifications.read');
+    Route::middleware('throttle:60,1')->prefix('notifications')->group(function (): void {
+        Route::get('/', [NotificationController::class, 'index'])->name('notifications.index');
+        Route::get('/unread-count', [NotificationController::class, 'unreadCount'])->name('notifications.unread-count');
+        Route::get('/recent', [NotificationController::class, 'recent'])->name('notifications.recent');
+        Route::post('/read-all', [NotificationController::class, 'markAllRead'])->name('notifications.read-all');
+        Route::post('/{id}/read', [NotificationController::class, 'markRead'])->name('notifications.read');
     });
 });
 
